dbcbcc77fd61cbcdf0912eee12a83a5b78f60220
Nicolas Boulet <nicolasboulet@live.fr>
2020-01-03 15:37:01 +0100

    Add new and create to controller + new view

--- Gemfile
+++ Gemfile
@@ -39,6 +39,9 @@ gem 'jbuilder', '~> 2.5'
 gem 'bootstrap'
 gem 'bootsnap', '>= 1.1.0', require: false
 
+#Add simpleform
+gem 'simple_form'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -3,4 +3,22 @@ class FlatsController < ApplicationController
   def index
     @flats = Flat.all
   end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    if Flat.create!(flat_params)
+      redirect_to flats_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:name, :adress, :description, :price_per_night, :number_of_guests)
+  end
 end
